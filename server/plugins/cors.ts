beb0f54a29ce1011e1e3c1f1d20dede54a0552e2
webdev-sdietrich <sascha@webagentur-dietrich.de>
2026-01-03 01:56:41 +0100

    fix: add further cors origin domain

--- server/plugins/cors.ts
+++ server/plugins/cors.ts
@@ -4,7 +4,7 @@ export default defineNitroPlugin((plugin) => {
   plugin.h3App.use(
     fromNodeMiddleware(
       cors({
-        origin: ['https://nu-be-au-exc.vercel.app', 'http://localhost:3000'],
+        origin: ['https://nu-be-au-exc.vercel.app', 'http://localhost:3000', 'https://nu-be-au-gpthp1tz6-sascha-dietrichs-projects.vercel.app'],
       }),
     ),
   )
